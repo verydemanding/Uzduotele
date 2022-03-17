3f4d5fe1aa3b5364e565dbe6e639c1637a9a3cb6
verydemanding <58390763+verydemanding@users.noreply.github.com>
2022-03-17 19:40:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 4 * 3.14 * 6371;
+            Console.WriteLine($"Rutulio pavirsiaus plotas: {x}");
+            
+        }
+    }
+}
